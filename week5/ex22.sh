47c4b26354f0aa56f19274378a1f80b48fef12b7
tasneem22 <tasneemsherif23@gmail.com>
2020-09-16 23:02:13 +0200

    Create ex22.sh

---
+++ week5/ex22.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+N=4
+
+if [ $# -eq 1 ]; then
+    N=$1;
+elif [ $# -gt 1 ]; then
+    echo "Usage: ${0} [execution count]"
+    exit
+fi
+
+echo 1 > ex22.txt
+
+for (( i = 1; i <= $N; i++ ))
+do
+	./add_lock.sh ex22.txt&
+	./add_lock.sh ex22.txt
+done
