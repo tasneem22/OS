dfd2da68e5b888c5ae7771bd9f7b6a9bbd577a33
tasneem22 <tasneemsherif23@gmail.com>
2020-09-30 23:12:37 +0300

    Create program.c

---
+++ week7/program.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+char *s = "Hello world!";
+
+int n;
+
+int main(int argc, char *argv[])
+{
+	const double PI = 3.14;
+	int x;
+	
+	printf("Got %d args!\n", argc);
+	printf("%s\n", s);
+	
+	return 0;
+}
